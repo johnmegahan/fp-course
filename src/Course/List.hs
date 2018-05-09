20a0807019f44808dd682ea23d00326979d30aa0
Johnny Megahan <johnmegahan@gmail.com>
2018-05-09 18:55:40 +0900

    done: Course.List#reverse

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -304,7 +304,7 @@ reverse ::
   List a
   -> List a
 reverse =
-  error "todo: Course.List#reverse"
+  foldLeft (flip (:.)) Nil
 
 -- | Produce an infinite `List` that seeds with the given value at its head,
 -- then runs the given function for subsequent elements
