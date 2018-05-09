23170a898c10351a46f6cffac801c0d3d0e025ff
Johnny Megahan <johnmegahan@gmail.com>
2018-05-09 18:55:10 +0900

    done: Course.List#lengthGT4

--- src/Course/List.hs
+++ src/Course/List.hs
@@ -284,8 +284,10 @@ find p =
 lengthGT4 ::
   List a
   -> Bool
-lengthGT4 =
-  error "todo: Course.List#lengthGT4"
+lengthGT4 (_ :. _ :. _:. _:. _:. _) =
+  True
+lengthGT4 _ =
+  False
 
 -- | Reverse a list.
 --
